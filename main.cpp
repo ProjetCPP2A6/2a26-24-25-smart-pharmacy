881e459b4a290478095c1d575bc460ca08d0c088
sirine277 <amri.sirine@esprit.tn>
2024-11-10 10:50:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,10 @@
-//#include "mainwindow.h"
-#include "employeeui.h"
-#include "connexion.h"
-
-#include <QApplication>
-#include <QMessageBox>
-
-int main(int argc, char *argv[])
+#include <iostream>
+#include <string>
+using namespace std;
+int mai
 {
-    QApplication a(argc, argv);
-    //MainWindow w;
-    employeeUI eUI;
-    connexion c;
-    bool test =c.createconnect();
-    if(test)
-    {
-        //  w.show();
-        eUI.exec();
-    }
-    else
-        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
-                              QObject::tr("connexion failed.\n"
-                                          "Click Cancel to exit."), QMessageBox::Cancel);
-
-    return a.exec();
+    cout<<"Hello World !"<<endl;
+    
+    return 0;
 }
 
