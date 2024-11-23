3f17eca492e88a565372ac41f0da384426b6f008
sirine <amri.sirine@esprit.tn>
2024-11-23 11:30:28 +0100

    Mise à jour de main.cpp avec la logique de connexion

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 //#include "mainwindow.h"
 #include "employeeui.h"
+#include "login.h"
 #include "connexion.h"
 
 #include <QApplication>
@@ -10,13 +10,13 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     //MainWindow w;
-    employeeUI eUI;
+    login l;
     connexion c;
     bool test =c.createconnect();
     if(test)
     {
         //  w.show();
-        eUI.exec();
+        l.exec();
     }
     else
         QMessageBox::critical(nullptr, QObject::tr("database is not open"),
@@ -24,15 +24,5 @@ int main(int argc, char *argv[])
                                           "Click Cancel to exit."), QMessageBox::Cancel);
 
     return a.exec();
-=======
-#include <iostream>
-#include <string>
-using namespace std;
-int mai
-{
-    cout<<"Hello World !"<<endl;
-    
-    return 0;
->>>>>>> 881e459b4a290478095c1d575bc460ca08d0c088
 }
 
