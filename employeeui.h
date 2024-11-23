77ef288b3393e73f718c958fbf281432ad795ef3
sirine <amri.sirine@esprit.tn>
2024-11-23 11:41:00 +0100

    Mise à jour de employeeUI.h avec des méthodes d'affichage et de mise à jour

--- employeeui.h
+++ employeeui.h
@@ -1,8 +1,8 @@
 #ifndef EMPLOYEEUI_H
 #define EMPLOYEEUI_H
-
 #include <QDialog>
 #include "employee.h"
+#include "congee.h"
 
 namespace Ui {
 class employeeUI;
@@ -37,9 +37,32 @@ private slots:
 
     void on_comboBox_tri_currentIndexChanged();
 
+
+
+
+
+
+
+
+
+    void on_Demande_conge_clicked();
+
+    void on_statistiqueButton_clicked();
+
+    void on_retour_clicked();
+
+    void on_stat_btn_clicked();
+
+    void on_ExportationButton_clicked();
+
+    void on_pushButton_22_clicked();
+
 private:
     Ui::employeeUI *ui;
+    congee *c;
     employee e;
+
+
 };
 
 #endif // EMPLOYEEUI_H
