542715a5a41c34f74200355b7d98394ca1944e45
ranimbenr12 <ranim.benrejeb@esprit.tn>
2024-11-24 10:13:07 +0100

    sms.cpp

---
+++ projectcpp/sms.cpp
@@ -0,0 +1,51 @@
+#include <QtNetwork>
+#include "sms.h"
+#include <QProcessEnvironment>  // Include this to work with environment variables
+
+QString twilio_phone_number = "+13212653837";
+
+sms::sms() {
+}
+
+void sms::sendSMS(const QString &to, const QString &message) {
+    QNetworkAccessManager *manager = new QNetworkAccessManager();
+
+    QUrl url("https://api.twilio.com/2010-04-01/Accounts/AC89558ed91916716c25828b95231ed38f/Messages.json");
+    QNetworkRequest request(url);
+
+    // Retrieve the Auth Token from the environment variable
+    QString authToken = QProcessEnvironment::systemEnvironment().value("TWILIO_AUTH_TOKEN");
+
+    // Check if the Auth Token is found in the environment variable
+    if (authToken.isEmpty()) {
+        qDebug() << "Error: Twilio Auth Token not found in environment variables.";
+        return;
+    }
+
+    // Construct credentials using the SID and Auth Token
+    QString credentials = "AC89558ed91916716c25828b95231ed38f:" + authToken;
+    QByteArray base64Credentials = credentials.toUtf8().toBase64();
+    request.setRawHeader("Authorization", "Basic " + base64Credentials);
+
+    QByteArray postData;
+    postData.append("From=" + twilio_phone_number.toUtf8());
+    postData.append("&To=" + to.toUtf8());
+    postData.append("&Body=" + message.toUtf8());
+
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+
+    // Make the request
+    QNetworkReply *reply = manager->post(request, postData);
+
+    QObject::connect(reply, &QNetworkReply::finished, [=]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            qDebug() << "SMS sent successfully";
+        } else {
+            qDebug() << "Failed to send SMS:" << reply->errorString();
+        }
+
+        reply->deleteLater();
+        manager->deleteLater();
+    });
+}
+s
