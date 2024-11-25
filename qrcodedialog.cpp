8fc1c7782fe59a1015ba59b24625deba5e759d2a
Abdelrahman <abdelrahmanhamoudi8@gmail.com>
2024-11-25 11:59:23 +0100

    ajouter qrcodedialog.cpp

--- qrcodedialog.cpp
+++ qrcodedialog.cpp
@@ -1,4 +1,4 @@
-#include "qrcodedialog.h"
+/*#include "qrcodedialog.h"
 #include "ui_qrcodedialog.h"
 
 #include <QMessageBox>
@@ -89,3 +89,4 @@ void QRCodeDialog::on_pushButton_clicked()
     close();
 }
 
+*/
