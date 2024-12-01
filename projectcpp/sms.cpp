ae26827a4b4e1dada2bad0a7eda8b4e407cde447
ranimbenr12 <ranim.benrejeb@esprit.tn>
2024-12-01 18:37:58 +0100

    Résolution des conflits dans sms.cpp

--- projectcpp/sms.cpp
+++ projectcpp/sms.cpp
@@ -1,5 +1,6 @@
 #include <QtNetwork>
 #include "sms.h"
+<<<<<<< HEAD
 #include <QProcessEnvironment>  // Include this to work with environment variables
 
 QString twilio_phone_number = "+13212653837";
@@ -7,12 +8,23 @@ QString twilio_phone_number = "+13212653837";
 sms::sms() {
 }
 
+=======
+
+
+QString twilio_phone_number = "+13212653837";
+
+
+sms::sms(){
+
+}
+/*/>>>>>>> ed90b17 (Created sms.cpp and sms.h)
 void sms::sendSMS(const QString &to, const QString &message) {
     QNetworkAccessManager *manager = new QNetworkAccessManager();
 
     QUrl url("https://api.twilio.com/2010-04-01/Accounts/AC89558ed91916716c25828b95231ed38f/Messages.json");
     QNetworkRequest request(url);
 
+<<<<<<< HEAD
     // Retrieve the Auth Token from the environment variable
     QString authToken = QProcessEnvironment::systemEnvironment().value("TWILIO_AUTH_TOKEN");
 
@@ -32,11 +44,29 @@ void sms::sendSMS(const QString &to, const QString &message) {
     postData.append("&To=" + to.toUtf8());
     postData.append("&Body=" + message.toUtf8());
 
+=======
+ /// ////////////////////////////////////////////////////////////////////// fi 3oth 1 t7ot SID w 2 t7ot Token
+    QString credentials = "AC89558ed91916716c25828b95231ed38f:d2b1154b8c5970866f24f3cbacfb50d8";
+    QByteArray base64Credentials = credentials.toUtf8().toBase64();
+    request.setRawHeader("Authorization", "Basic " + base64Credentials);
+
+
+    QByteArray postData;
+    postData.append("From="+twilio_phone_number.toUtf8());
+    postData.append("&To=" + to.toUtf8());
+    postData.append("&Body=" + message.toUtf8());
+
+
+>>>>>>> ed90b17 (Created sms.cpp and sms.h)
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
 
     // Make the request
     QNetworkReply *reply = manager->post(request, postData);
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> ed90b17 (Created sms.cpp and sms.h)
     QObject::connect(reply, &QNetworkReply::finished, [=]() {
         if (reply->error() == QNetworkReply::NoError) {
             qDebug() << "SMS sent successfully";
@@ -44,8 +74,15 @@ void sms::sendSMS(const QString &to, const QString &message) {
             qDebug() << "Failed to send SMS:" << reply->errorString();
         }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> ed90b17 (Created sms.cpp and sms.h)
         reply->deleteLater();
         manager->deleteLater();
     });
 }
+<<<<<<< HEAD
 s
+=======
+>>>>>>> ed90b17 (Created sms.cpp and sms.h)*/
